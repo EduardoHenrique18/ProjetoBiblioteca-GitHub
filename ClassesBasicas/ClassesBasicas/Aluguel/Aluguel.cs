5f8a4e21cf19e0cd8a629c5c856df11687afdafe
Eduardo H <eduardo.henrique204@gmail.com>
2019-04-18 18:46:44 -0300

    Classes basicas

---
+++ ClassesBasicas/ClassesBasicas/Aluguel/Aluguel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesBasicas.Livro;
+using ClassesBasicas.Usuario;
+
+
+namespace ClassesBasicas.Aluguel
+{
+    class Aluguel
+    {
+        private char dtEmprestimo;
+        private int dtEntrega;
+
+        private ICollection<Livro> livro;
+
+        private Usuario usuario;
+
+        public Aluguel()
+        {
+            this.Livro = new Livro();
+            this.Usuario = new Usuario();
+        }
+
+        public char DtEmprestimo
+        {
+            get
+            {
+                return dtEmprestimo;
+            }
+
+            set
+            {
+                dtEmprestimo = value;
+            }
+        }
+
+        public int DtEntrega
+        {
+            get
+            {
+                return dtEntrega;
+            }
+
+            set
+            {
+                dtEntrega = value;
+            }
+        }
+
+        public ICollection<Livro> Livro
+        {
+            get
+            {
+                return livro;
+            }
+
+            set
+            {
+                livro = value;
+            }
+        }
+
+        public Usuario Usuario
+        {
+            get
+            {
+                return usuario;
+            }
+
+            set
+            {
+                usuario = value;
+            }
+        }
+
+       
+
+        public void cadastrarAluguel()
+        {
+
+        }
+
+        public void listarAluguel()
+        {
+
+        }
+
+        public void deletarAluguel()
+        {
+
+        }
+    }
+}
---
+++ ClassesBasicas/ClassesBasicas/Pagamento/Pagamento.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesBasicas.Usuario;
+
+namespace ClassesBasicas.Pagamento
+{
+    class Pagamento
+    {
+        private int idPagamento;
+        private float valorPagamento;
+
+        private Usuario usuario;
+
+        public Pagamento()
+        {
+            this.usuario = new Usuario();
+        }
+
+        public int IdPagamento
+        {
+            get
+            {
+                return idPagamento;
+            }
+
+            set
+            {
+                idPagamento = value;
+            }
+        }
+
+        public float ValorPagamento
+        {
+            get
+            {
+                return valorPagamento;
+            }
+
+            set
+            {
+                valorPagamento = value;
+            }
+        }
+
+        public Usuario Usuario
+        {
+            get
+            {
+                return usuario;
+            }
+
+            set
+            {
+                usuario = value;
+            }
+        }
+
+        public void cadastrarPagamento()
+        {
+
+        }
+
+        public void listarPagamento()
+        {
+
+        }
+
+        public void deletarPagamento()
+        {
+
+        }
+    }
+}
